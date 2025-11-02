94c735ecb213b7f92a7f0eb6ebc5feaba06af468
NeuralVFX <neuralvfx@gmail.com>
2025-11-02 14:58:23 +0000

    Adding train sh script

---
+++ train_libre_flux.sh
@@ -0,0 +1,17 @@
+python train_libre_flux.py \
+  --pretrained_model_name_or_path="jimmycarter/LibreFLUX" \
+  --image_encoder_path="laion/CLIP-ViT-H-14-laion2B-s32B-b79K" \
+  --data_json_file="laion2b-squareish-1024px_subset/data.json" \
+  --data_root_path="laion2b-squareish-1024px_subset" \
+  --val_data_json_file="test_dataset/data.json" \
+  --val_data_root_path="test_dataset" \
+  --mixed_precision="bf16" \
+  --resolution=512 \
+  --train_batch_size=4 \
+  --dataloader_num_workers=4 \
+  --learning_rate=5e-05 \
+  --weight_decay=0.01 \
+  --quantize \
+  --output_dir="output_dir" \
+  --save_steps=1000 \
+  --val_steps=1000 
